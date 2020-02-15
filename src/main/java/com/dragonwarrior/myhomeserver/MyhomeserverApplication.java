095ccd3487aa4180d55801b034f9eb833afb2e29
dragonwarrior <1121118610@qq.com>
2020-02-16 01:14:02 +0800

    Initial commit

---
+++ src/main/java/com/dragonwarrior/myhomeserver/MyhomeserverApplication.java
@@ -0,0 +1,13 @@
+package com.dragonwarrior.myhomeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyhomeserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyhomeserverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dragonwarrior/myhomeserver/MyhomeserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dragonwarrior.myhomeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyhomeserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
